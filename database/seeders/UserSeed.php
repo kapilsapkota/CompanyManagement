3b86d5d305cedc185467ec7544e5c0860640fc7a
kapilsapkota <searchin.kapil@gmail.com>
2023-07-20 21:42:38 +1000

    Added UserSeed with first user

---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //seed first user with given data
+         User::create([
+            'name'      => 'Admin',
+            'email'     => 'admin@admin.com',
+            'password'  => Hash::make('password')
+        ]);
+
+    }
+}
