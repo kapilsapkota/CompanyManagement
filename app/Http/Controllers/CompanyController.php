c1b01d48f48c6dd4e8e0fa729f35dccaf52a2b5d
kapilsapkota <searchin.kapil@gmail.com>
2023-07-20 23:43:54 +1000

    Added Company and Employee Controller and CompanyRequest

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CompanyRequest;
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $companies = Company::paginate(10);
+        return view('companies.index', compact('companies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('companies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CompanyRequest $request)
+    {
+        $validatedData = $request->validated();
+        $id = Company::max('id') + 1;
+        if($validatedData->hasFile('logo_upload')){
+            $filename = 'logo_' . $id . '.png';
+            $imagePath = 'public/'. $filename;
+            Storage::put($imagePath, $validatedData['logo_upload']);
+            $validatedData['logo'] = $filename
+        }
+        $company = Company::create($validatedData);
+
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
